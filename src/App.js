f89ff9221098509af571f1f388d172c9ca38f9a6
CodeSandbox <support@codesandbox.io>
2023-07-22 15:40:52 +0000

    "Initial commit"

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Ashish Kuldeep</h1>
+      <h2>Lets Deploy this code on Github </h2>
+    </div>
+  );
+}
